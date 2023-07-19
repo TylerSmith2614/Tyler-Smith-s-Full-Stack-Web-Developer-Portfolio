1f96fe3cae4c25292049abc42005f314cc514586
tylersmith2614 <tylersmith2614@gmail.com>
2023-07-18 21:57:19 -0600

    created sidebar header  with nav-bar

---
+++ tyler-smith-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,49 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+// import LogoT from '../../assets/images/LogoT'
+import LogoT2 from '../../assets/images/LogoT2.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoT2} alt="Tyler's Coding Logo"></img>
+    </Link>
+    <nav>
+      <NavLink exact="true" activeClassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="white" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeClassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="white" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeClassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="white" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com">
+          <FontAwesomeIcon icon={faLinkedin} color="white" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://www.github.com">
+          <FontAwesomeIcon icon={faGithub} color="white" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
