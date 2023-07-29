57d1d72886c17a10b063d3e4e690c2f3bf1a9161
tylersmith2614 <tylersmith2614@gmail.com>
2023-07-28 18:49:04 -0600

    started portfolio page

--- tyler-smith-portfolio/src/components/Portfolio/index.js
+++ tyler-smith-portfolio/src/components/Portfolio/index.js
@@ -1,10 +1,69 @@
-import Loader from "react-loaders"
+import Loader from 'react-loaders'
+import React from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import portfolioData from '../../Data/portfolio.json'
 
 const Portfolio = () => {
-  return(
-<>
-<Loader type="ball-pulse-rise" />
-</>
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  const renderPortfolio = (portfolio) => {
+    return (
+      <div className="images-container">
+        {portfolio.map((port, idx) => {
+          return (
+            <div className="image-box" key={idx}>
+              <img
+                src={port.cover}
+                className="portfolio-image"
+                alt="portfolio"
+              />
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button
+                  className="btn"
+                  onClick={() => window.open(port.deployedUrl)}
+                >
+                  Deployed App
+                </button>
+                <button
+                  className="btn"
+                  onClick={() => window.open(port.githubRepoUrl)}
+                >
+                  Github Repo
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={'My Portfolio'.split('')}
+            idx={15}
+          />
+        </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
+      </div>
+      <Loader type="ball-pulse-rise" />
+    </>
   )
 }
 
